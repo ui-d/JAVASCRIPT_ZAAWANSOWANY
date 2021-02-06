51e688381a7a0313c5a26870d097f470f7bfe38d
ui-d <dawiddeveloper@gmail.com>
2021-02-06 23:56:18 +0100

    filter truthy vals

--- filter.js
+++ filter.js
@@ -1,6 +1,8 @@
 let array = [12, 5, 8, 130, 44, 3, 333];
 let stringArray = ["Flamingo", "bird", "ui developer"];
 
+let wirdArray = [1, 2, 3, 0, false, true, "Flamingo", Symbol(3)];
+
 let isBigEnough = (value) => {
   return value >= 10;
 };
@@ -24,3 +26,5 @@ array.filter(isNotEven); /*?*/
 array.filter(isDivisableByThree); /*?*/
 
 stringArray.filter(hasFourChars); /*?*/
+
+wirdArray.filter(Boolean); /*?*/
