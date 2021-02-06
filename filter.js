e9ddfa98cc6fd781505743369059cfad68baf838
ui-d <dawiddeveloper@gmail.com>
2021-02-06 23:55:05 +0100

    filter method

--- filter.js
+++ filter.js
@@ -1,7 +1,26 @@
-function isBigEnough(value) {
+let array = [12, 5, 8, 130, 44, 3, 333];
+let stringArray = ["Flamingo", "bird", "ui developer"];
+
+let isBigEnough = (value) => {
   return value >= 10;
-}
+};
 
-var array = [12, 5, 8, 130, 44].filter(isBigEnough);
+let isNotEven = (value) => {
+  return value % 2;
+};
 
-array;
+let isDivisableByThree = (value) => {
+  return !(value % 3);
+};
+
+let hasFourChars = (value) => {
+  return value.length === 4;
+};
+
+array.filter(isBigEnough); /*?*/
+
+array.filter(isNotEven); /*?*/
+
+array.filter(isDivisableByThree); /*?*/
+
+stringArray.filter(hasFourChars); /*?*/
