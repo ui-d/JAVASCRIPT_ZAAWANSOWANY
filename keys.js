5666a9740608cd8dc2c70f79a39485470b3859f6
ui-d <dawiddeveloper@gmail.com>
2021-02-09 23:13:14 +0100

    keys() array method

---
+++ keys.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4];
+
+let keys = arr.keys(); /*?*/
+
+keys.next(); /*?*/
+keys.next(); /*?*/
+keys.next(); /*?*/
+keys.next(); /*?*/
+keys.next(); /*?*/
+
+Object.keys(arr); /*?*/ // array of strings
+Object.values(arr); /*?*/
