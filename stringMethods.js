e3aedeb862323435fd4003c7a8dcd66c24aaa9d3
ui-d <dawiddeveloper@gmail.com>
2021-02-11 23:06:20 +0100

    string methods

---
+++ stringMethods.js
@@ -0,0 +1,11 @@
+// String.fromCharCode()
+// String.fromCodePoint()
+// String.prototype.charAt()
+// String.prototype.charCodeAt()
+
+let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJ";
+
+String.fromCharCode(33, 54, 57); /*?*/
+String.fromCodePoint(9731, 9733, 9842, 0x2f804); /*?*/
+str.charAt(3); /*?*/
+str.charCodeAt(2); /*?*/
