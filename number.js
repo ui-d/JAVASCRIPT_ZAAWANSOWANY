87d85c2a1f3b9229d2337763a979dcf5d0780469
ui-d <dawiddeveloper@gmail.com>
2021-02-12 23:15:14 +0100

    number functions

---
+++ number.js
@@ -0,0 +1,11 @@
+let integer = 21;
+let float = 3.5;
+let longFloat = 3.4443454563454;
+
+Number.isInteger(float); /*?*/
+float.toFixed(); /*?*/
+longFloat.toFixed(); /*?*/
+integer.toExponential(); /*?*/
+let numToString = typeof integer.toString(); /*?*/
+let value = typeof float.valueOf(); /*?*/
+longFloat.toPrecision(4); /*?*/
