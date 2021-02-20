730792d1e2f61fce82e729f33851fe010b8446ae
ui-d <dawiddeveloper@gmail.com>
2021-02-20 21:10:25 +0100

    get custom property value from css

--- setProperty.js
+++ setProperty.js
@@ -1,6 +1,10 @@
 let paragraph = document.querySelector(".paragraph");
 console.log(paragraph);
 
+let zIndex = getComputedStyle(document.documentElement).getPropertyValue(
+  "--zIndex"
+);
+
 function changeColors(el) {
   el.style.setProperty("--backgroundColor", "rgba(255,255,255)");
   el.style.setProperty("--borderColor", "blue");
@@ -9,3 +13,5 @@ function changeColors(el) {
 paragraph.addEventListener("click", function (e) {
   changeColors(e.currentTarget);
 });
+
+console.log(zIndex);
