7fdb587800b1de02b774fe25c017733e6c4f5b61
ui-d <dawiddeveloper@gmail.com>
2021-02-21 22:40:17 +0100

    nullish coalescing

---
+++ nullishcoalescing.js
@@ -0,0 +1,8 @@
+const value1 = 1;
+const value2 = 23;
+const value3 = null;
+const value4 = undefined;
+
+const result = value1 || value2;
+const restult2 = value3 ?? value4;
+console.log(restult2);
