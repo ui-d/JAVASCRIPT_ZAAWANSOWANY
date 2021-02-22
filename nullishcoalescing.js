401370a2bcf70cad73e42c628ba86b125315b00f
ui-d <dawiddeveloper@gmail.com>
2021-02-22 07:18:08 +0100

    nullish coalescing

--- nullishcoalescing.js
+++ nullishcoalescing.js
@@ -6,6 +6,21 @@ const value4 = undefined;
 const result = value1 || value2;
 const restult2 = value3 ?? value4;
 
+let firstName = null;
+let lastName = null;
+let nickName = "Supercoder";
+
+// shows the first defined value:
+console.log(firstName ?? lastName ?? nickName ?? "Anonymous"); // Supercoder
+
+let height = null;
+let width = null;
+
+// important: use parentheses
+let area = (height ?? 100) * (width ?? 50);
+
+console.log(area); // 5000
+
 // nullish assignment
 
 let a = {
