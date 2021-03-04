66fd5a187bc28361a29f1f846a52c9091b99e345
ui-d <dawiddeveloper@gmail.com>
2021-03-04 11:02:03 +0100

    jsdoc class example

--- jsdoc.js
+++ jsdoc.js
@@ -12,3 +12,29 @@ function createPlayer(name, age, isActive) {
 }
 
 createPlayer("Flamingo", 35, true);
+
+// Class example
+
+class Player {
+  /**
+   * @param {string} name Player's nickname
+   * @param {number} age Player's age'
+   * @param {boolean} isActive Player's active status
+   */
+  constructor(name, age, isActive) {
+    this.name = name;
+    this.age = age;
+    this.isActive = isActive;
+  }
+}
+
+let newPlayer = new Player("Dawid", "21", false);
+
+/**
+ * Output details about player
+ *
+ * @param {Player} player
+ */
+function createNewPlayer(player) {
+  console.log(player);
+}
