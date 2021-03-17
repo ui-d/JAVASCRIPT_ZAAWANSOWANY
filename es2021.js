d8ac55bcb0b336bcad42f1a1114f3e474c036744
ui-d <dawiddeveloper@gmail.com>
2021-03-17 22:51:39 +0100

    new operators

--- es2021.js
+++ es2021.js
@@ -13,3 +13,22 @@ const refObj = new WeakRef({ name: "John" });
 const refObj = new WeakRef({ name: "John" });
 const obj = refObj.deref();
 console.log(obj.name); // John
+
+
+// Logical Assignment Operator
+
+let x = 10;
+let y = 21;
+
+
+y &&= x; // Equivalent: x && (x = y)
+
+// Logical OR assignment
+
+
+let w = undefined;
+let z = 15;
+
+w ||= z;  // Equivalent: x || (x = y)
+
+
