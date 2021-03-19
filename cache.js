8fafac1dcca79e61f3427a4e0f64c7980b5e8a1c
ui-d <dawiddeveloper@gmail.com>
2021-03-19 23:50:53 +0100

    cache options

--- cache.js
+++ cache.js
@@ -1,3 +1,14 @@
 caches.open('test-cache').then(function(cache) {
   console.log(cache)
 });
+
+
+// cache options
+
+const options = {
+  method: "GET",
+  headers: new Headers({
+      'Content-Type': 'text/html'
+  }),
+}  
+newCache.add(new Request('/cats.json', options))
