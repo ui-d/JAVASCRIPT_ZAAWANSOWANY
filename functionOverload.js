b258b070710692b99eb8b24bcc07f02d41397528
ui-d <dawiddeveloper@gmail.com>
2021-03-20 22:02:09 +0100

    function overload

---
+++ functionOverload.js
@@ -0,0 +1,24 @@
+function sum() {
+  switch (arguments.length) {
+  case 0:
+      console.log('You have 0 arguments');
+      break;
+  case 1:
+      console.log('Pass at least two arguments');
+      break;
+  default:
+      let result = 0;
+      let length = arguments.length;
+  
+      for (i = 0; i < length; i++) {  
+          result = result + arguments[i];  
+      }  
+      console.log(result);
+      break;
+  }
+}
+
+sum();
+sum(5); 
+sum(5, 9);    
+sum(1, 2, 3, 4, 5, 6, 7, 8, 9);
