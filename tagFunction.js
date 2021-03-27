bec31461ad2c307a00a612eb7d354d37267280f6
ui-d <dawiddeveloper@gmail.com>
2021-03-27 09:51:30 +0100

    tag funcions'

--- tagFunction.js
+++ tagFunction.js
@@ -1,5 +1,5 @@
-var name = 'Alfred';
-var age = 47;
+const name = 'Alfred';
+const age = 47;
  
 function greet(){
   console.log(arguments[0]);
@@ -7,3 +7,16 @@ function greet(){
   console.log(arguments[2]);
 }
 greet`I'm ${name}. I'm ${age} years old.`;
+
+
+
+const objects = [{ name: "Flamingo", age: 33}, {name: "Blackbird", age: 21}];
+
+function returnArgs(str, arg1, arg2) {
+    console.log(str + arg1 + arg2)
+}
+
+
+objects.map((el)=> {
+    returnArgs`This is ${el.name} of age ${el.age}`
+})
